e3bfad200d851d9b9d3ce2ea65780c35d6edad58
Aqvic <public@aqvic.com>
2016-02-19 09:55:10 +0300

    Simplest AnalogIn -> PWM

--- Led4All.ino
+++ Led4All.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+/* 
+ *  https://github.com/Aqvic/Led4All
+*/
 
-}
+const byte inPins[4] = {1, 2, 3, 4};
+const byte outPins[4] = {1, 2, 3, 4};
+
+void setup() {}
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    for(int i=0; i<4; i++){
+        analogWrite(outPins[i], map(analogRead(inPins[i]), 0, 1023, 0, 255));
+    }
 }
